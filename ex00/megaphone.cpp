7f29349f31f14fd696a309ef4a081deff5be8e82
nschutz <nschutz@student.42wolfsburg.de>
2024-01-22 14:01:01 +0100

    CPP 01 ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-	cout << "Hello World!";
+int main(int argc, char **argv) {
+	int c = 0;
+	int d = 1;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 0;
+	}
+	while (argv[d])
+	{
+		while (argv[d][c]){
+			std::cout << (char )toupper(argv[d][c]);
+			c++;
+		}
+		d++;
+		c = 0;
+	}
+	std::cout << std::endl;
 	return 0;
 }
