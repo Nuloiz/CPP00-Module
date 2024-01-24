9e9ca1008b744f09c4b5f57a3fca87b6f098234c
nschutz <nschutz@student.42wolfsburg.de>
2024-01-24 12:29:16 +0100

    CPP 01 ex01 Fixing losing contact, when list is full and new contact is invalid

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -9,85 +9,90 @@ static void	move_contacts(int num, PhoneBook *phonebook){
 }
 
 void	add_contact(PhoneBook *phonebook){
-	int num	= 0;
+	int num = -1;
+	std::string	fn;
+	std::string	ln;
+	std::string	nn;
+	std::string pn;
+	std::string	ds;
 	std::string input;
-	if (phonebook->number == 8){
-		while (num < 7){
-			move_contacts(num, phonebook);
-			num++;
-		}
-		(phonebook->number)--;
-	}
+
 	std::cout << "First Name:";
-	std::getline (std::cin, phonebook->contacts[phonebook->number].first_name);
-	if (!phonebook->contacts[phonebook->number].first_name[0]){
+	std::getline (std::cin, fn);
+	if (!fn[0]){
 		std::cout << "Error Fail, Field can not be empty. Returned to Menu\n";
-		return ;
+		return ;;
 	}
-	num = -1;
-	while (phonebook->contacts[phonebook->number].first_name[++num]){
-		if (!isalpha(phonebook->contacts[phonebook->number].first_name[num])){
+	while (fn[++num]){
+		if (!isalpha(fn[num])){
 			std::cout << "Error Fail, First Name hast to be only letters. Returned to Menu\n";
 			return ;
 		}
 	}
 	std::cout << "Last Name:";
-	std::getline (std::cin, phonebook->contacts[phonebook->number].last_name);
-	if (!phonebook->contacts[phonebook->number].last_name[0]){
+	std::getline (std::cin, ln);
+	if (!ln[0]){
 		std::cout << "Error Fail, Field can not be empty. Returned to Menu\n";
 		return ;
 	}
 	num = -1;
-	while (phonebook->contacts[phonebook->number].last_name[++num]){
-		if (!isalpha(phonebook->contacts[phonebook->number].last_name[num])){
+	while (ln[++num]){
+		if (!isalpha(ln[num])){
 			std::cout << "Error Fail, Last Name hast to be only letters. Returned to Menu\n";
 			return ;
 		}
 	}
 	std::cout << "Nickname:";
-	std::getline (std::cin, phonebook->contacts[phonebook->number].nickname);
-	if (!phonebook->contacts[phonebook->number].nickname[0]){
+	std::getline (std::cin, nn);
+	if (!nn[0]){
 		std::cout << "Error Fail, Field can not be empty. Returned to Menu\n";
 		return ;
 	}
 	num = -1;
-	while (phonebook->contacts[phonebook->number].nickname[++num]){
-		if (!isascii(phonebook->contacts[phonebook->number].nickname[num])){
+	while (nn[++num]){
+		if (!isascii(nn[num])){
 			std::cout << "Error Fail, Nickname hast to be only ascii characters. Returned to Menu\n";
 			return ;
 		}
 	}
 	std::cout << "Phone-Number:";
-	std::getline (std::cin, phonebook->contacts[phonebook->number].phone_number);
+	std::getline (std::cin, pn);
 	num = -1;
-	if (!phonebook->contacts[phonebook->number].phone_number[0]) {
+	if (!pn[0]) {
 		std::cout << "Error Fail, Field can not be empty. Returned to Menu\n";
-		return;
+		return ;
 	}
-	while (phonebook->contacts[phonebook->number].phone_number[++num]){
-		if (!isdigit(phonebook->contacts[phonebook->number].phone_number[num])){
-			std::cout << "Error Fail, Phone Number hast to be only digits. Returned to Menu\n";
-			return ;
-		}
-	}
-	while (phonebook->contacts[phonebook->number].phone_number[++num]){
-		if (!isdigit(phonebook->contacts[phonebook->number].phone_number[num])){
+	while (pn[++num]){
+		if (!isdigit(pn[num])){
 			std::cout << "Error Fail, Phone Number hast to be only digits. Returned to Menu\n";
 			return ;
 		}
 	}
 	std::cout << "Darkest Secret:";
-	std::getline (std::cin, phonebook->contacts[phonebook->number].secret);
-	if (!phonebook->contacts[phonebook->number].secret[0]){
+	std::getline (std::cin, ds);
+	if (!ds[0]){
 		std::cout << "Error Fail, Field can not be empty. Returned to Menu\n";
 		return ;
 	}
 	num = -1;
-	while (phonebook->contacts[phonebook->number].secret[++num]){
-		if (!isascii(phonebook->contacts[phonebook->number].secret[num])){
+	while (ds[++num]){
+		if (!isascii(ds[num])){
 			std::cout << "Error Fail, darkest secret hast to be only ascii characters. Returned to Menu\n";
 			return ;
 		}
 	}
+	if (phonebook->number == 3){
+		num = 0;
+		while (num < 2){
+			move_contacts(num, phonebook);
+			num++;
+		}
+		(phonebook->number)--;
+	}
+	phonebook->contacts[phonebook->number].first_name = fn;
+	phonebook->contacts[phonebook->number].last_name = ln;
+	phonebook->contacts[phonebook->number].nickname = nn;
+	phonebook->contacts[phonebook->number].phone_number = pn;
+	phonebook->contacts[phonebook->number].secret = ds;
 	(phonebook->number)++;
 }
